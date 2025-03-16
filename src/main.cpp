66138f59238b50c1636a86491b7a4652f1db3ce8
ash-hashtag <hashtag438@gmail.com>
2025-03-16 18:57:59 +0530

    streaming response

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,25 @@
 #include "boost/asio.hpp"
 #include "boost/asio/awaitable.hpp"
+#include "boost/asio/deadline_timer.hpp"
 #include "boost/asio/posix/stream_descriptor.hpp"
 #include "boost/beast.hpp"
+#include "boost/beast/http/buffer_body.hpp"
 #include "boost/beast/http/chunk_encode.hpp"
+#include "boost/beast/http/empty_body.hpp"
+#include "boost/beast/http/serializer.hpp"
 #include "boost/beast/http/status.hpp"
 #include "boost/beast/http/string_body.hpp"
+#include "boost/beast/http/write.hpp"
 #include "boost/system/detail/error_code.hpp"
 #include "boost/system/system_error.hpp"
+#include <cerrno>
 #include <csignal>
 #include <cstdint>
 #include <cstring>
 #include <fcntl.h>
 #include <stdio.h>
+#include <string>
+#include <sys/stat.h>
 
 namespace http = boost::beast::http;
 namespace asio = boost::asio;
@@ -25,7 +33,6 @@ asio::awaitable<void> handle_request_async(tcp::socket socket,
   http::request<http::string_body> req;
 
   co_await http::async_read(socket, buffer, req, asio::use_awaitable);
-  http::response<http::string_body> res;
   std::string file_path;
 
   if (req.target().starts_with("/")) {
@@ -39,51 +46,100 @@ asio::awaitable<void> handle_request_async(tcp::socket socket,
   int fd = open(file_path.c_str(), O_RDONLY | O_NONBLOCK);
 
   if (fd == -1) {
+    http::response<http::string_body> res;
     res.result(http::status::not_found);
     res.set(http::field::content_type, "text/plain");
     res.body() = "404 Not Found";
+    res.prepare_payload();
+    co_await http::async_write(socket, res, asio::use_awaitable);
     printf("file %s does not exist\n", file_path.c_str());
   } else {
-    printf("Reading file %s\n", file_path.c_str());
-    res.result(http::status::ok);
-    res.set(http::field::content_type, "text/plain");
 
-    std::string body;
+    struct stat info;
+    if (fstat(fd, &info) == -1) {
+      http::response<http::string_body> res;
+      res.result(http::status::not_found);
+      res.set(http::field::content_type, "text/plain");
+      res.body() = "404 Not Found";
+      res.prepare_payload();
+      co_await http::async_write(socket, res, asio::use_awaitable);
+      printf("reading file %s has some problem errno %d \n", file_path.c_str(),
+             errno);
+      co_return;
+    }
+
+    if (!S_ISREG(info.st_mode)) {
+      http::response<http::string_body> res;
+      res.result(http::status::not_found);
+      res.set(http::field::content_type, "text/plain");
+      res.body() = "404 Not Found";
+      res.prepare_payload();
+      co_await http::async_write(socket, res, asio::use_awaitable);
+      printf(" %s aint a file ", file_path.c_str());
+      co_return;
+    }
+
+    printf("Reading file %s with length %ld\n", file_path.c_str(),
+           info.st_size);
+    http::response<http::buffer_body> res;
+
+    res.result(http::status::ok);
+    res.version(11);
+    res.set(http::field::content_type, "text/plain");
+    // res.set(http::field::transfer_encoding, "chunked");
+    res.set(http::field::content_length, std::to_string(info.st_size));
+
+    res.body().data = nullptr;
+    res.body().more = true;
+
+    http::response_serializer<http::buffer_body, http::fields> sr(res);
+
+    co_await http::async_write_header(socket, sr, asio::use_awaitable);
 
     file.assign(fd);
 
-    char buffer[4096];
+#define BUFFER_CAPACITY 4096
+
+    char buffer[BUFFER_CAPACITY];
 
     while (true) {
-
       try {
-        auto bytes_read = co_await file.async_read_some(asio::buffer(buffer),
-                                                        asio::use_awaitable);
+
+        // simulating throttle
+        asio::deadline_timer timer(ioc, boost::posix_time::seconds(1));
+        co_await timer.async_wait(asio::use_awaitable);
+
+        auto bytes_read = co_await file.async_read_some(
+            asio::buffer(buffer, sizeof(buffer)), asio::use_awaitable);
 
         if (bytes_read == 0) {
           break;
         }
 
-        body.append(buffer, bytes_read);
+        res.body().data = buffer;
+        res.body().size = bytes_read;
+        res.body().more = true;
 
+        co_await http::async_write(socket, sr, asio::use_awaitable);
 
       } catch (const boost::system::system_error &e) {
+        if (e.code() == http::error::need_buffer) {
+          continue;
+        }
         if (e.code() == boost::asio::error::eof) {
           break;
         } else {
-
-          printf("Request handling error %s\n", e.what());
+          printf("request hander error %s\n", e.what());
+          break;
         }
       }
     }
-    
-    res.body() = body;
+
+    res.body().data = nullptr;
+    res.body().more = false;
+    co_await http::async_write(socket, sr, asio::use_awaitable);
   }
-
-  res.prepare_payload();
-  co_await http::async_write(socket, res, asio::use_awaitable);
 }
-
 
 asio::awaitable<void> accept_connections(tcp::acceptor acceptor,
                                          asio::io_context &ioc) {
@@ -106,7 +162,6 @@ void start_server(uint16_t port) {
 
   ioc.run();
 }
-
 
 int main() {
 
